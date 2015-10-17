8cf3ce6a7453e2d7f99a83860e526386b85d1983
way0750 <bruce0750@gmail.com>
2015-10-16 22:48:38 -0700

    can now pick random enemy

--- client/watchout.js
+++ client/watchout.js
@@ -1,7 +1,7 @@
 
 var width = 1400;
 var height = 580;
-var animateSpeed = 1000;
+var animateSpeed = 1500;
 var enemySize = 20;
 var playerSize = 20;
 var highestScore = 0;
@@ -9,39 +9,53 @@ var curScore = 0;
 var frameSoFar = 0;
 var numCollisons = 0;
 var collisionDistance = 30;
+var enemyFaces = ["001.png", "buster.png", "kenny.png", "kyle.png"];
 
 
 var board = d3.select('.board').append('svg').attr({width : width, height : height});
-
 var enemyArray = [], enemies;
+
+function selectRandomEnemy () {
+  return enemyFaces[Math.floor( Math.random() * enemyFaces.length )];
+}
 
 function generateEnemies (n){
   for (var i = 0; i<n; ++i){
     enemyArray.push([i, true]);
   }
   enemies = board.selectAll('circle').data(enemyArray);
+
+
+
   enemies.enter()
          .append('image')
          .classed('enemy', true)
-         .attr('xlink:href', 'assets/carman.png')
+         .attr('xlink:href', function () {
+           return 'assets/' + selectRandomEnemy();
+         })
          .attr({
           'height':playerSize*4+'px', 
           'width':playerSize*4+'px'
         })
-         .attr( "x", function(){return Math.random()* width}) 
-         .attr( "y", function(){return Math.random()* height});
+         .attr( "x", function(){return Math.random()* width;}) 
+         .attr( "y", function(){return Math.random()* height;});
+
+
 
   enemies = board.selectAll('.enemy');
 }
 
 generateEnemies(20);
 
-var player1 = board.append('circle');
+var player1 = board.append('image');
 player1.classed('player', true)
-  .attr('r', playerSize + "px").attr('fill', "blue")
-  .attr({'stroke': 'black', 'stroke-width':'5px'})
-  .attr('cx', width/2)
-  .attr('cy', height/2);
+  .attr('xlink:href', 'assets/carman.png')
+  .attr({
+    'height':playerSize*4+'px', 
+    'width':playerSize*4+'px'
+  })
+  .attr('x', 25)
+  .attr('y', 25);
 
 
 var dragBehavior = d3.behavior.drag();
@@ -50,10 +64,10 @@ dragBehavior.on('drag', function () {
   var xPos = d3.event.x, yPos = d3.event.y;
   var node = d3.select(this);
   if (playerSize<=xPos && xPos+playerSize*2 <=width) {
-      d3.select(this).attr('cx', xPos);
+      d3.select(this).attr('x', xPos-playerSize*1.8);
   }
   if (playerSize <= yPos && yPos+playerSize*2 <=height){
-    d3.select(this).attr('cy', yPos);
+    d3.select(this).attr('y', yPos-playerSize*1.8);
   }
 });
 
@@ -67,8 +81,8 @@ var animate = function(){
                   .transition()
                   .ease('cubic-bezier')
                   .duration(animateSpeed)
-                  .attr( "x", function(){return Math.random()* width}) 
-                  .attr("y", function(){return Math.random()* height});
+                  .attr( "x", function(){return Math.random()* width;}) 
+                  .attr("y", function(){return Math.random()* height;});
 
   setTimeout(animate, animateSpeed);
 };
@@ -82,16 +96,16 @@ function playExplosionFactory(){
     var explosion = board.append('image')
             .attr('xlink:href', 'assets/explosion.gif')
             .attr({
-              'x': xPos-playerSize*5, 
-              'y':yPos-playerSize*5, 
+              'x': xPos-playerSize*2.3, 
+              'y':yPos-playerSize*2.3, 
               'height':playerSize*10+'px', 
               'width':playerSize*10+'px'
             });
       readyToExplode = false;
-      setTimeout(function(){explosion.remove()}, 750)
+      setTimeout(function(){explosion.remove();}, 750);
       setTimeout(function(){readyToExplode = true;}, 500); 
     }
-  }
+  };
   return explode;
 }
 var playExplosion = playExplosionFactory();
@@ -111,8 +125,8 @@ function collisionCheck () {
     var enemyX = +(d3.select(this).attr('x').slice(0, -2));
     var enemyY = +(d3.select(this).attr('y').slice(0, -2));
     var player1 = d3.select('.player');
-    var player1x = parseInt(player1.attr('cx'));
-    var player1y = parseInt(player1.attr('cy'));
+    var player1x = parseInt(player1.attr('x'));
+    var player1y = parseInt(player1.attr('y'));
     // console.log(enemyX);
 
     if (data[1] && Math.abs(player1x - enemyX) < collisionDistance && Math.abs(player1y - enemyY) < collisionDistance){    // collision happened
